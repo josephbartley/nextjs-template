423f85ac94ee679749a190ce154cad76c8eed29d
Joseph Bartley <joseph@josephbartley.dev>
2022-06-12 00:39:32 -0500

    initial version

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withPreact = require('next-plugin-preact')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+}
+
+module.exports = withPreact(nextConfig)
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react'
+
+const Home: FunctionComponent = () => {
+	return <></>
+}
+
+export default Home
